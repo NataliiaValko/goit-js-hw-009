539251868a1f28b5c1e5ed46a7647bcc48c27986
Nataliia <75252511+NataliiaValko@users.noreply.github.com>
2022-10-30 19:44:10 +0200

    add scrypt

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let intervalId = null;
+const SWITCH_INTERVAL = 1000;
+
+const onStartSwitchInterval = () => {
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, SWITCH_INTERVAL);
+};
+
+const onStopSwitchColor = () => {
+  clearInterval(intervalId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+};
+
+stopBtnRef.disabled = true;
+startBtnRef.addEventListener('click', onStartSwitchInterval);
+stopBtnRef.addEventListener('click', onStopSwitchColor);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+const handleSubmit = e => {
+  e.preventDefault();
+  const formsEl = e.currentTarget.elements;
+  console.log('formsEl :', formsEl);
+
+  const amount = Number(formsEl.amount.value);
+  let delay = Number(formsEl.delay.value);
+  const step = Number(formsEl.step.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
-}
+};
+
+refs.form.addEventListener('submit', handleSubmit);
